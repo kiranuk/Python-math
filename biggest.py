ca5685958a33b0aa927ede467da715307239c935
kiranuk <Kiran6475@icloud.com>
2019-04-22 11:34:41 +0530

    Refactors

--- biggest.py
+++ biggest.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-n = int(input("Enter the number of elements: ")
 
 def biggest(l):
+    n = len(l)
     a = []
-    b = int(input("Enter the elements: ")
-    for i in range(l, n+1):
-        a.append(b)
-        a.sort()
-    print("largest number is: ",a[n - 1])
+    for i in l:
+        a.append(i)
+    a.sort()
+    print(a)
+    print("largest number is: ",a[-1])
         
+biggest([23,1,4,50,6])
